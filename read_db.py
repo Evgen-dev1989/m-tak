cb6c7750a140ec59c602a3612f948c27ad32b91e
Evgen-dev1989 <81103637+Evgen-dev1989@users.noreply.github.com>
2024-10-31 15:19:52 +0200

    +

--- read_db.py
+++ read_db.py
@@ -5,8 +5,9 @@ async def main():
 
     conn = await asyncpg.connect(user='postgres', password='1111',database='m-tak', host='localhost')
 
-    a = await conn.fetch('SELECT * FROM products_data;')
-    print(a)
+    a = await conn.fetch("SELECT product_id, time FROM products_data ORDER BY product_id ASC;")
+    with open("output.txt", "w", encoding="utf-8") as file:
+        file.write(str(a))
 
 
 if __name__ == '__main__': 
